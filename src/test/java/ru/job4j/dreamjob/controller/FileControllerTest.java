f6fa623a10efd6eebc6f94c4300396e8504d54f2
AndrewBurUU <andriv80@gmail.com>
2023-01-31 18:47:36 +0800

    2. Тестируем VacancyController [#504867 #386658]

--- src/test/java/ru/job4j/dreamjob/controller/FileControllerTest.java
+++ src/test/java/ru/job4j/dreamjob/controller/FileControllerTest.java
@@ -1,19 +1,13 @@
 package ru.job4j.dreamjob.controller;
 
 import org.junit.jupiter.api.*;
-import org.mockito.ArgumentCaptor;
-import org.springframework.http.*;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.ui.ConcurrentModel;
 import org.springframework.web.multipart.MultipartFile;
 import ru.job4j.dreamjob.dto.FileDto;
-import ru.job4j.dreamjob.model.File;
-import ru.job4j.dreamjob.service.CityService;
 import ru.job4j.dreamjob.service.FileService;
 
 import java.util.*;
 
-import static java.time.LocalDateTime.now;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -32,17 +26,23 @@ class FileControllerTest {
         testFile = new MockMultipartFile("testFile.img", new byte[] {1, 2, 3});
     }
 
-    @Disabled
     @Test
     public void whenRequestFileThenGetFile() throws Exception {
         var fileDto = new FileDto(testFile.getOriginalFilename(), testFile.getBytes());
         when(fileService.getFileById(1)).thenReturn(Optional.of(fileDto));
 
-        var model = new ConcurrentModel();
         var view = fileController.getById(1);
-        var actualFileDto = view.getBody();
 
-        assertThat(view).isEqualTo("200 OK OK,[B@224b4d61,[]");
-/**        assertThat(fileDto).usingRecursiveComparison().isEqualTo(view);*/
+        assertThat(view.toString()).contains("200");
     }
+
+    @Test
+    public void whenRequestNoFileThenError() throws Exception {
+        when(fileService.getFileById(1)).thenReturn(Optional.empty());
+
+        var view = fileController.getById(1);
+
+        assertThat(view.toString()).contains("404");
+    }
+
 }
